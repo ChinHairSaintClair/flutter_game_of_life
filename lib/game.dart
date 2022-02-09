abc880ecaba0e3985eae6ed1531df27b5d18754d
Anro <anro.devs@gmail.com>
2022-02-10 00:21:48 +0200

    Refactoring

---
+++ lib/game.dart
@@ -0,0 +1,310 @@
+import 'dart:math';
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'game_constants.dart';
+
+class Game extends StatefulWidget {
+  const Game({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<Game> createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+  bool _isRunning = false;
+  List<List<bool>> _tracker = _generateList();
+
+  static const int _milliseconds = DEFAULT_TICK_INTERVAL;
+  static const int _gridSize = DEFAULT_GRID_SIZE;
+
+  Stream _stream = Stream.periodic(const Duration(milliseconds: _milliseconds));
+  late StreamSubscription _periodicSub;
+
+  @override
+  void initState() {
+    _periodicSub = _stream.listen(
+        (_) => _isRunning ? _buildNextIteration() : print('empty tick'));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildContent(),
+      floatingActionButton: _buildFloatingActionButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+
+  _buildAppBar() {
+    return AppBar(
+      leading: IconButton(
+        icon: const Icon(Icons.help),
+        tooltip: WIKI_LINK_TOOLTIP,
+        onPressed: () => _openWikiLink(),
+      ),
+      title: Text(widget.title),
+      centerTitle: true,
+      actions: [
+        InkWell(
+          child: CircleAvatar(
+            child: _isRunning
+                ? const Icon(Icons.pause)
+                : const Icon(Icons.play_arrow),
+          ),
+          onTap: () => _isRunning ? _pauseGame() : _runGame(),
+        ),
+      ],
+    );
+  }
+
+  _buildContent() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Expanded(
+          child: _buildDescription(),
+        ),
+        _buildGameGrid(),
+        Expanded(child: _buildControls()),
+      ],
+    );
+  }
+
+  Center _buildDescription() {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: const [
+          Text(
+            GAME_INSTRUCTION_HEADER,
+            style: TextStyle(fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
+          Text(GAME_INSTRUCTION_SUB_HEADER),
+          Text(GAME_INSTRUCTION_1),
+          Text(GAME_INSTRUCTION_2),
+        ],
+      ),
+    );
+  }
+
+  AspectRatio _buildGameGrid() {
+    return AspectRatio(
+      aspectRatio: 1,
+      child: Row(
+        children: [
+          ..._tracker
+              .asMap()
+              .map((outerIndex, outer) {
+                return MapEntry(
+                  outerIndex,
+                  Expanded(
+                    child: Column(
+                      children: [
+                        ...outer
+                            .asMap()
+                            .map((innerIndex, inner) {
+                              return MapEntry(
+                                innerIndex,
+                                _buildGridItem(inner, outerIndex, innerIndex),
+                              );
+                            })
+                            .values
+                            .toList(),
+                      ],
+                    ),
+                  ),
+                );
+              })
+              .values
+              .toList(),
+        ],
+      ),
+    );
+  }
+
+  Expanded _buildGridItem(bool isAlive, int rowIndex, int columnIndex) {
+    return Expanded(
+      child: InkWell(
+        child: Center(
+          child: Container(
+            color: isAlive ? Colors.black : Colors.white,
+          ),
+        ),
+        onTap: () => !_isRunning
+            ? _manuallyToggleBlockState(
+                rowIndex,
+                columnIndex,
+              )
+            : null,
+      ),
+    );
+  }
+
+  Row _buildControls() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(TICK_DESCRIPTION_HEADER),
+            OutlinedButton(
+              child: const Text('${_milliseconds / 1000} $TICK_MEASUREMENT'),
+              onPressed: () => _setGenerationInterval(),
+            ),
+          ],
+        ),
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(GRID_SIZE_DESCRIPTION_HEADER),
+            OutlinedButton(
+              child: const Text('$_gridSize x $_gridSize $GRID_MEASUREMENT'),
+              onPressed: () => _setGridSize(),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  _buildFloatingActionButton() {
+    return !_isRunning
+        ? FloatingActionButton(
+            onPressed: _reset,
+            tooltip: RESET,
+            child: const Icon(Icons.threesixty),
+          )
+        : null;
+  }
+
+  _openWikiLink() async {
+    if (await canLaunch(GAME_OF_LIFE_URL)) {
+      await launch(GAME_OF_LIFE_URL);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('$UNABLE_TO_LAUNCH_URL $GAME_OF_LIFE_URL')));
+    }
+  }
+
+  static List<List<bool>> _generateList() {
+    return List.generate(_gridSize,
+        (index) => List.generate(_gridSize, (i) => Random().nextBool()));
+  }
+
+  _manuallyToggleBlockState(int x, y) {
+    setState(() {
+      _tracker[x][y] = !_tracker[x][y];
+    });
+  }
+
+  _runGame() async {
+    if (_isRunning) {
+      return;
+    }
+
+    setState(() {
+      _isRunning = true;
+    });
+  }
+
+  _buildNextIteration() {
+    var _nextGeneration = List.generate(_tracker.length,
+        (index) => List.generate(_tracker[index].length, (_) => false));
+
+    for (int x = 1; x < _tracker.length - 1; x++) {
+      for (int y = 1; y < _tracker[x].length - 1; y++) {
+        bool isAlive = _tracker[x][y];
+
+        bool newState;
+        if (isAlive && _willSurvive(x, y)) {
+          newState = true;
+        } else if (!isAlive && _canBeResurrected(x, y)) {
+          newState = true;
+        } else {
+          // All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+          newState = false;
+        }
+
+        _nextGeneration[x][y] = newState;
+      }
+    }
+
+    setState(() {
+      _tracker = _nextGeneration;
+    });
+  }
+
+  _willSurvive(int x, int y) {
+    // Any live cell with two or three live neighbours survives.
+    var count = _determineAliveNeighbourCount(x, y);
+    return count == 2 || count == 3;
+  }
+
+  _canBeResurrected(int x, int y) {
+    // Any dead cell with three live neighbours becomes a live cell.
+    return _determineAliveNeighbourCount(x, y) == 3;
+  }
+
+  _determineAliveNeighbourCount(int x, int y) {
+    int aliveCount = 0;
+    // End pointer is not included in the result
+    List<List<bool?>> taken = [..._tracker.getRange(x - 1, x + 2)];
+    for (var element in taken) {
+      taken[taken.indexOf(element)] = element.getRange(y - 1, y + 2).toList();
+    }
+
+    for (int i = 0; i < taken.length; i++) {
+      for (int j = 0; j < taken[i].length; j++) {
+        if (i == 1 && j == 1) {
+          continue; // Ignore your current position
+        }
+
+        if (taken[i][j] ?? false) {
+          aliveCount++;
+        }
+      }
+    }
+
+    return aliveCount;
+  }
+
+  _pauseGame() async {
+    if (!_isRunning) {
+      return;
+    }
+
+    setState(() {
+      _isRunning = false;
+    });
+  }
+
+  void _reset() {
+    setState(() {
+      _isRunning = false;
+      _tracker = _generateList();
+    });
+  }
+
+  _setGenerationInterval() {
+    // TODO:
+  }
+
+  _setGridSize() {
+    // TODO:
+  }
+
+  @override
+  void dispose() {
+    _periodicSub.cancel();
+    super.dispose();
+  }
+}
---
+++ lib/game_constants.dart
@@ -0,0 +1,18 @@
+const WIKI_LINK_TOOLTIP = "Wikipedia - Conway's game of life";
+const GAME_OF_LIFE_URL = "https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life";
+const UNABLE_TO_LAUNCH_URL = 'Could not launch';
+
+const GAME_INSTRUCTION_HEADER = 'Customize by clicking on the grid items.';
+const GAME_INSTRUCTION_SUB_HEADER = 'This can be done during the following:';
+const GAME_INSTRUCTION_1 = '1. At the start of a new game.';
+const GAME_INSTRUCTION_2 = '2. Whilst the game is paused.';
+
+const TICK_DESCRIPTION_HEADER = 'Generation interval:';
+const DEFAULT_TICK_INTERVAL = 50;
+const TICK_MEASUREMENT = 'seconds';
+
+const GRID_SIZE_DESCRIPTION_HEADER = 'Grid size:';
+const DEFAULT_GRID_SIZE = 50;
+const GRID_MEASUREMENT = 'blocks';
+
+const RESET = 'Reset';
